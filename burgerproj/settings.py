cae00247704e4cfef93c73b8a9e424adfafacd61
catherinekimani <catherinekimani882@gmail.com>
2022-07-25 23:46:03 +0300

    Ft: Create Database

--- burgerproj/settings.py
+++ burgerproj/settings.py
@@ -83,8 +83,9 @@ WSGI_APPLICATION = 'burgerproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'burger',
+        'PASSWORD':'kaykatekruz1',
     }
 }
 
