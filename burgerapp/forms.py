80bca2498041cf0ba987d4fe45674ea4fc8091d7
catherinekimani <catherinekimani882@gmail.com>
2022-07-29 15:19:51 +0300

    Ft: Create profile update form

--- burgerapp/forms.py
+++ burgerapp/forms.py
@@ -21,3 +21,9 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['user_bio','user_profile']
+        
+class ProfileUpdateForm(forms.ModelForm):
+    user_bio = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
+    class Meta:
+        model = Profile
+        fields = ['user_bio','user_contact','location','user_profile']
